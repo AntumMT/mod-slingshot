ff29db5f464254cf588e34f7d72093f42c528fef
AntumDeluge <antumdeluge@gmail.com>
2017-06-01 02:38:02 -0700

    Add weaker wooden slingshot

--- register.lua
+++ register.lua
@@ -38,4 +38,17 @@ minetest.register_tool('slingshot:slingshot', {
 })
 
 
+-- A weaker slingshot
+slingshot.register('wood', {
+	description = 'Wooden slingshot',
+	damage_groups = {fleshy=2},
+	velocity = 10,
+	recipe = {
+		{'default:stick', '', 'default:stick'},
+		{'', 'default:stick', ''},
+		{'', 'default:stick', ''},
+	},
+})
+
+
 minetest.register_alias('slingshot', 'slingshot:slingshot')
