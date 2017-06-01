8508c843e55ea804898b5682aabed7ba8684a852
AntumDeluge <antumdeluge@gmail.com>
2017-06-01 00:06:03 -0700

    Change spacing of equations

--- functions.lua
+++ functions.lua
@@ -2,20 +2,20 @@
 
 
 function slingshot.on_use(itemstack, user)
-	local veloc=15
+	local veloc = 15
 	local pos = user:getpos()
-	local upos={x=pos.x, y=pos.y+2, z=pos.z}
+	local upos = {x=pos.x, y=pos.y+2, z=pos.z}
 	local dir = user:get_look_dir()
-	local item=itemstack:to_table()
-	local mode=minetest.deserialize(item['metadata'])
-	if mode == nil then mode=1 else mode=mode.mode end
+	local item = itemstack:to_table()
+	local mode = minetest.deserialize(item['metadata'])
+	if mode == nil then mode = 1 else mode = mode.mode end
 
-	local item=user:get_inventory():get_stack('main', user:get_wield_index()+mode):get_name()
+	local item = user:get_inventory():get_stack('main', user:get_wield_index()+mode):get_name()
 	if item == '' then return itemstack end
 	local e = minetest.add_item({x=pos.x, y=pos.y+2, z=pos.z}, item)
 	e:setvelocity({x=dir.x*veloc, y=dir.y*veloc, z=dir.z*veloc})
 	e:setacceleration({x=dir.x*-3, y=-5, z=dir.z*-3})
-	e:get_luaentity().age=hook_tmp_time
+	e:get_luaentity().age = hook_tmp_time
 	table.insert(hook_tmp_throw, {ob=e, timer=2, user=user:get_player_name()})
 
 	if item == 'slingshot:slingshot' then
