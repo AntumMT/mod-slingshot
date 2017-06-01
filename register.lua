e41df53594c71f598f065a61a0a4ad7ee7dcb9be
AntumDeluge <antumdeluge@gmail.com>
2017-06-01 04:20:18 -0700

    Use 'ingredient' in place of 'recipe' for registering

--- register.lua
+++ register.lua
@@ -20,11 +20,7 @@ slingshot.register('slingshot', {
 	description = 'Slingshot',
 	damage_groups = {fleshy=4},
 	velocity = 15,
-	recipe = {
-		{'default:steel_ingot', '', 'default:steel_ingot'},
-		{'', 'default:steel_ingot', ''},
-		{'', 'default:steel_ingot', ''},
-	},
+	ingredient = 'default:steel_ingot',
 	aliases = {
 		'slingshot',
 	},
@@ -36,11 +32,7 @@ slingshot.register('wood', {
 	description = 'Wooden slingshot',
 	damage_groups = {fleshy=2},
 	velocity = 10,
-	recipe = {
-		{'default:stick', '', 'default:stick'},
-		{'', 'default:stick', ''},
-		{'', 'default:stick', ''},
-	},
+	ingredient = 'group:stick',
 	aliases = {
 		'wood_slingshot',
 	}
