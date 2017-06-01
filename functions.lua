cf0a36d74c47ba8d58d6f629ba8cb9d24dbc16b0
AntumDeluge <antumdeluge@gmail.com>
2017-05-31 23:34:41 -0700

    Move function 'slingshot_onuse' to 'functions.lua':
    - Make attribute of 'slingshot' object
    - Rename to 'slingshot.on_use'

---
+++ functions.lua
@@ -0,0 +1,28 @@
+-- Functions for slingshot mod
+
+
+function slingshot.on_use(itemstack, user)
+	local veloc=15
+	local pos = user:getpos()
+	local upos={x=pos.x, y=pos.y+2, z=pos.z}
+	local dir = user:get_look_dir()
+	local item=itemstack:to_table()
+	local mode=minetest.deserialize(item['metadata'])
+	if mode == nil then mode=1 else mode=mode.mode end
+
+	local item=user:get_inventory():get_stack('main', user:get_wield_index()+mode):get_name()
+	if item == '' then return itemstack end
+	local e = minetest.add_item({x=pos.x, y=pos.y+2, z=pos.z}, item)
+	e:setvelocity({x=dir.x*veloc, y=dir.y*veloc, z=dir.z*veloc})
+	e:setacceleration({x=dir.x*-3, y=-5, z=dir.z*-3})
+	e:get_luaentity().age=hook_tmp_time
+	table.insert(hook_tmp_throw, {ob=e, timer=2, user=user:get_player_name()})
+
+	if item == 'slingshot:slingshot' then
+	itemstack:set_wear(9999999)
+	end
+
+	user:get_inventory():remove_item('main', item)
+	minetest.sound_play('hook_throw', {pos=pos, gain = 1.0, max_hear_distance = 5,})
+	return itemstack
+end
--- init.lua
+++ init.lua
@@ -15,31 +15,15 @@ else
 	hook_tmp_time=hook_tmp_time-10
 end
 
-local function slingshot_onuse(itemstack, user)
-	local veloc=15
-	local pos = user:getpos()
-	local upos={x=pos.x,y=pos.y+2,z=pos.z}
-	local dir = user:get_look_dir()
-	local item=itemstack:to_table()
-	local mode=minetest.deserialize(item["metadata"])
-	if mode==nil then mode=1 else mode=mode.mode end
 
-	local item=user:get_inventory():get_stack("main", user:get_wield_index()+mode):get_name()
-	if item=="" then return itemstack end
-	local e=minetest.add_item({x=pos.x,y=pos.y+2,z=pos.z},item)
-	e:setvelocity({x=dir.x*veloc, y=dir.y*veloc, z=dir.z*veloc})
-	e:setacceleration({x=dir.x*-3, y=-5, z=dir.z*-3})
-	e:get_luaentity().age=hook_tmp_time
-	table.insert(hook_tmp_throw,{ob=e,timer=2,user=user:get_player_name()})
+local scripts = {
+	'functions',
+}
 
-	if item=="hook:slingshot" then
-	itemstack:set_wear(9999999)
-	end
-
-	user:get_inventory():remove_item("main", item)
-	minetest.sound_play("hook_throw", {pos=pos, gain = 1.0, max_hear_distance = 5,})
-	return itemstack
+for index, script in ipairs(scripts) do
+	dofile(slingshot.modpath .. '/' .. script .. '.lua')
 end
+
 
 
 
