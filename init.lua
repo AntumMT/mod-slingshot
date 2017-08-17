5301218970f0d52ae61aa914d68b6d6d02542501
Jordan Irwin <antumdeluge@gmail.com>
2017-08-16 17:41:14 -0700

    Small optimization

--- init.lua
+++ init.lua
@@ -4,10 +4,7 @@ slingshot.modname = minetest.get_current_modname()
 slingshot.modpath = minetest.get_modpath(slingshot.modname)
 
 -- Requires rubber band for slingshot craft recipe
-slingshot.require_rubber_band = minetest.settings:get_bool('slingshot.require_rubber_band')
-if slingshot.require_rubber_band == nil then
-	slingshot.require_rubber_band = true
-end
+slingshot.require_rubber_band = minetest.settings:get_bool('slingshot.require_rubber_band') ~= false
 
 if minetest.settings:get_bool('log_mods') then
 	minetest.log('action', '[slingshot] Require rubber band: ' .. tostring(slingshot.require_rubber_band))
