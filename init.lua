0761f2a0a112affe8451ce2a46ce66ceb7ac2f2a
Jordan Irwin <antumdeluge@gmail.com>
2026-06-08 22:57:39 -0700

    Use mod logger for logging

--- init.lua
+++ init.lua
@@ -1,32 +1,12 @@
 
-slingshot = {}
-slingshot.modname = core.get_current_modname()
+slingshot = {
+	modname = core.get_current_modname(),
+	log = function() end
+}
 slingshot.modpath = core.get_modpath(slingshot.modname)
 
-function slingshot.log(lvl, msg)
-	if not msg then
-		msg = lvl
-		lvl = nil
-	end
-
-	if not msg and not lvl then
-		lvl = "error"
-		msg = "no log arguments supplied"
-	end
-
-	msg = "[" .. slingshot.modname .. "] " .. msg
-	if lvl == "debug" then
-		if not slingshot.debug then return end
-
-		msg = "[DEBUG] " .. msg
-		lvl = nil
-	end
-
-	if not lvl then
-		core.log(msg)
-	else
-		core.log(lvl, msg)
-	end
+if core.global_exists("register_mod_logger") then
+	register_mod_logger(slingshot)
 end
 
 
